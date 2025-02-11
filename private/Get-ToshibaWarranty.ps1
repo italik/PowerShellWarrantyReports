1afb2af104d809fe6596f415445fa7fe4795fd0b
robm82 <47445676+robm82@users.noreply.github.com>
2025-02-11 15:02:37 +0000

    Update Get-ToshibaWarranty.ps1

--- private/Get-ToshibaWarranty.ps1
+++ private/Get-ToshibaWarranty.ps1
@@ -1,21 +1,21 @@
 function get-ToshibaWarranty
 ([Parameter(Mandatory = $true)]$SourceDevice, [Parameter(Mandatory = $false)] [string]$ModelNumber, [Parameter(Mandatory = $false)] [string]$client) {
     if ($script:ExcludeToshiba -ne $True) {
-    $today = Get-Date -Format yyyy-MM-dd
-    $APIURL = "http://support.toshiba.com/support/warrantyResults?sno=" + $SourceDevice + "&mpn=" + $modelnumber
-    $Req = Invoke-RestMethod -Uri $APIURL -Method get
-    if ($req.commonBean) {
-        #$warlatest = $Req.EndDate | sort-object | select-object -last 1 
-        $WarrantyState = if ($req.commonBean.warrantyExpiryDate -le $today) { "Expired" } else { "OK" }   
-        $WarObj = [PSCustomObject]@{
-            'Serial'                = $req.commonBean.serialNumber
-            'Warranty Product name' = ($Req.serviceTypes.Carry.svcDesc -replace '<[^>]+>', '')
-            'StartDate'             = [DateTime]::ParseExact($($req.commonBean.warOnsiteDate), 'yyyy-MM-dd HH:mm:ss.f', [Globalization.CultureInfo]::CreateSpecificCulture('en-NL'))
-            'EndDate'               = [DateTime]::ParseExact($($req.commonBean.warrantyExpiryDate), 'yyyy-MM-dd HH:mm:ss.f', [Globalization.CultureInfo]::CreateSpecificCulture('en-NL'))
-            'Warranty Status'       = $WarrantyState
-            'Client'                = $Client
+        $today = Get-Date -Format yyyy-MM-dd
+        $APIURL = "http://support.toshiba.com/support/warrantyResults?sno=" + $SourceDevice + "&mpn=" + $modelnumber
+        $Req = Invoke-RestMethod -Uri $APIURL -Method get
+        if ($req.commonBean) {
+            #$warlatest = $Req.EndDate | sort-object | select-object -last 1 
+            $WarrantyState = if ($req.commonBean.warrantyExpiryDate -le $today) { "Expired" } else { "OK" }   
+            $WarObj = [PSCustomObject]@{
+                'Serial'                = $req.commonBean.serialNumber
+                'Warranty Product name' = ($Req.serviceTypes.Carry.svcDesc -replace '<[^>]+>', '')
+                'StartDate'             = [DateTime]::ParseExact($($req.commonBean.warOnsiteDate), 'yyyy-MM-dd HH:mm:ss.f', [Globalization.CultureInfo]::CreateSpecificCulture('en-NL'))
+                'EndDate'               = [DateTime]::ParseExact($($req.commonBean.warrantyExpiryDate), 'yyyy-MM-dd HH:mm:ss.f', [Globalization.CultureInfo]::CreateSpecificCulture('en-NL'))
+                'Warranty Status'       = $WarrantyState
+                'Client'                = $Client
+            }
         }
-    }
     }
     else {
         $WarObj = [PSCustomObject]@{
@@ -28,6 +28,4 @@ function get-ToshibaWarranty
         }
     }
     return $WarObj
- 
  }
-}
